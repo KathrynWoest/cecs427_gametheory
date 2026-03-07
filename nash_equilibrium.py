27f400bdf3ae1e34363f5d2e9dd8d7f0f9bee265
KathrynWoest <kathryn.woest@gmail.com>
2026-03-07 14:49:28 -0800

    Brainstorm plan for iterating through every path

--- nash_equilibrium.py
+++ nash_equilibrium.py
@@ -31,6 +31,23 @@ def equilibrium(graph, vehicles, start, end):
             for i in range(1, vehicles + 1):
                 lowest_pe += (a * i) + b
 
+            # try each path with v1. then for each option for v1, try every path for v2. and so on
+            # ex with 2 vehicles and 3 paths:
+                # for path 1, assign v1 to it. then try path 1, 2, and 3 for v2 and keep lowest pe
+                # for path 2, assign v1 to it. then try path 1, 2, and 3 for v2 and keep lowest pe of all of them
+
+            # for tomorrow:
+                # use intertools.combinations_with_replacement(range(len(all_paths)), vehicles) to get an iterable item that picks all possible combinations for vehicles distributed over number of paths
+                    # list(itertools.c...) with return a list of tuples like so: [(1,2), (1,3), (1,4)]. in this case, each tuple represents paths chosen, so (v1, v2, v3, ...) where vi will have the number of the path chosen
+                    # then go through each resulting tuple and count the number of vehicles that chose each path, and store that in a tuple that is the size of the paths, not the vehicles. so (2, 1, 3) indicates 2 vehicles picked path 1, 1 picked 2, etc
+                    # put all of those tuples in a list
+                    # we now have a list of every possible numerical way the vehicles can be distributed across the paths
+                # use the method i used above to calculate pe to calculate it for every single tuple in that list
+                    # extract to a new function possibly, and do the same for intertools distribution
+                    # use enumerate() to collect the location in the tuple and the value in the tuple
+                # if the pe is lower than the current highest, save it and save the distribution tuple to return
+                # start the pe off with float("inf"), representing infinity
+
     except Exception as e:
         print(f"Calculation of travel equilibrium was terminated because there was an error. Error:", e)
         return
